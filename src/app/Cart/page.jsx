914eb49a84794246605bad1c8aaaa10693a76c17
Donpollo6916 <pmlaser25@gmail.com>
2025-07-23 18:09:43 -0500

    Add prices on page and cart css

--- src/app/Cart/page.jsx
+++ src/app/Cart/page.jsx
@@ -3,20 +3,46 @@ import { useCart } from "@/context/CartContext";
 
 export default function CartPage() {
   const { cart, removeFromCart } = useCart();
-
+  const total = cart.reduce((sum, item) => sum + (item.precio || 0), 0);
   return (
-    <div>
-      <h1>Carrito de compras</h1>
-      {cart.length === 0 ? (
-        <p>El carrito está vacío</p>
-      ) : (
-        cart.map((item) => (
-          <div key={item._id}>
-            <span>{item.nombre}</span>
-            <button onClick={() => removeFromCart(item._id)}>Eliminar</button>
+    <section>
+      <div>
+        <h1>Carrito de compras</h1>
+        {cart.length === 0 ? (
+          <p>El carrito está vacío</p>
+        ) : (
+          cart.map((item) => (
+            <div key={item._id}>
+              <span>{item.nombre}</span>
+              <button onClick={() => removeFromCart(item._id)}>Eliminar</button>
+            </div>
+          ))
+        )}
+      </div>
+      <div>
+        <h1>Total:</h1>
+        {cart.length === 0 ? (
+          <p>0</p>
+        ) : (
+          <div>
+            <span>{total} $</span>
           </div>
-        ))
-      )}
-    </div>
+        )}
+      </div>
+      <div>
+        <button disabled={cart.length === 0}>
+          Pagar
+        </button>
+        <button onClick={() => clearCart()} disabled={cart.length === 0}>
+          Vaciar carrito
+        </button>
+        <div>
+          <button onClick={() => window.location.href = '/whatsapp'}>
+            Contactar por WhatsApp
+          </button>
+        </div>
+      </div>
+
+    </section>
   );
 }
